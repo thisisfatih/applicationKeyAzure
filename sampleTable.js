812afd3bcf7ffe2d6d7bb118b6a9b687a1b24b30
Fatih <thisisfatih@users.noreply.github.com>
2016-10-11 23:32:12 +0300

    Create sampleTable.js

---
+++ sampleTable.js
@@ -0,0 +1,17 @@
+var azureMobileApps = require('azure-mobile-apps'),
+    validateApiKey = require('../validateApiKey');
+
+// Create a new table definition
+var table = azureMobileApps.table();
+
+// Access should be anonymous so that unauthenticated users are not rejected
+// before our custom validateApiKey middleware runs.
+table.access = 'anonymous';
+
+// validate api key header prior to execution of any table operation
+    table.use(validateApiKey, table.execute);
+// to require api key authentication for only one operation (in this case insert)
+// instead of table.use(validateApiKey, table.execute) use:
+// table.insert.use(validateApiKey, table.operation);
+
+module.exports = table;
